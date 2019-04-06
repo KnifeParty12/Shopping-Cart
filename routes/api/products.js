1c57c6fbd3e5b5d75f3f6b5c3912f1bdd362611f
KnifeParty12 <flashfunk01@gmail.com>
2019-04-06 23:45:21 +0530

    Added User endpoint to API
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

---
+++ routes/api/products.js
@@ -0,0 +1,14 @@
+const Product = require('../../db');
+const route = require('express').Router();
+
+route.get('/',(req,res)=>{
+    //Get all products
+
+});
+
+route.post('/',(req,res)=>{});
+
+
+
+
+exports = module.exports = route;
